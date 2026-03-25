4a90aeb40cdbc5ee2df316e565e4d646adecf4c8
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-26 01:00:16 +0530

    Add member in organization api is added

--- server/routes/org.ts
+++ server/routes/org.ts
@@ -3,11 +3,13 @@ import CreateOrganization from '../controllers/Organization/Create';
 import authenticateToken from '../middlewares/authToken';
 import Orgme from '../controllers/Organization/orgme';
 import UpdateOrg from '../controllers/Organization/update';
+import AddMember from '../controllers/Organization/addMember';
 
 const Orgrouter=express.Router();
 
 Orgrouter.post('/',authenticateToken,CreateOrganization);
 Orgrouter.patch('/',authenticateToken,UpdateOrg);
 Orgrouter.get('/me',authenticateToken,Orgme);
+Orgrouter.post('/member',authenticateToken,AddMember);
 
 export default Orgrouter;
