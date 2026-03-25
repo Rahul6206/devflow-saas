08d5ed9d5542965a6bcfbbe480b1eee60825498e
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-25 23:31:00 +0530

    add get orginazition data api

---
+++ server/controllers/Organization/orgme.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import { AuthRequest } from '../../utils/RequestType';
+import { prisma } from '../../lib/prisma';
+
+const Orgme= async(req:AuthRequest,res:express.Response)=>{
+    const usrid=req.user.id;
+    if(!usrid) return res.status(401).json({message: "user id not found"})
+    try {
+        const orgUserexist=await prisma.user.findUnique({
+            where:{
+                id: usrid
+            }
+        })
+
+        if(!orgUserexist) return res.status(401).json({message: "user not exist"});
+        if(!orgUserexist.organizationId) return res.status(401).json({message: "organization not found"});
+        const OrgData= await prisma.organization.findUnique({
+            where:{
+                id: orgUserexist.organizationId
+            }
+        })
+        res.status(200).json({organizationId: OrgData?.id, name: OrgData?.name})
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(404).json({ message: error.message })
+        }
+    }
+
+}
+
+export default Orgme;
--- server/routes/org.ts
+++ server/routes/org.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import CreateOrganization from '../controllers/Organization/Create';
 import authenticateToken from '../middlewares/authToken';
+import Orgme from '../controllers/Organization/orgme';
 
 const Orgrouter=express.Router();
 
 Orgrouter.post('/',authenticateToken,CreateOrganization);
+Orgrouter.get('/me',authenticateToken,Orgme);
 
 export default Orgrouter;
