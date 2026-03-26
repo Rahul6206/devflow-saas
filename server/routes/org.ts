327560339a0a4a1bd9ede7b4b611f33354537cec
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-26 17:33:47 +0530

    Delete Organization api was created but not tested

--- server/routes/org.ts
+++ server/routes/org.ts
@@ -5,11 +5,13 @@ import Orgme from '../controllers/Organization/orgme';
 import UpdateOrg from '../controllers/Organization/update';
 import AddMember from '../controllers/Organization/addMember';
 import GetOrgMembers from '../controllers/Organization/GetallMembers';
+import DeleteOrg from '../controllers/Organization/deleteOrg';
 
 const Orgrouter=express.Router();
 
 Orgrouter.post('/',authenticateToken,CreateOrganization);
 Orgrouter.patch('/',authenticateToken,UpdateOrg);
+Orgrouter.delete('/',authenticateToken,DeleteOrg);
 Orgrouter.get('/me',authenticateToken,Orgme);
 Orgrouter.post('/member',authenticateToken,AddMember);
 Orgrouter.get('/member',authenticateToken,GetOrgMembers);
