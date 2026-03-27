e523d643c583e18da8c6963dea7e587389c9854b
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-27 23:58:07 +0530

    Get all Task with Project Id Api was Completed

--- server/routes/task.ts
+++ server/routes/task.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import authenticateToken from "../middlewares/authToken";
 import CreateTask from "../controllers/Task/CreateTask";
+import GetTasks from "../controllers/Task/GetTask";
 
 const TaskRoute=express.Router();
 
-TaskRoute.post('/',authenticateToken,CreateTask)
+TaskRoute.post('/',authenticateToken,CreateTask);
+TaskRoute.get('/',authenticateToken,GetTasks);
 
 export default TaskRoute;
