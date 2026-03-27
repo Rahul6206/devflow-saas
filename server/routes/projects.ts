3004994b3c6dee2124c739d4c2e941b8ba5df3f6
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-27 17:12:43 +0530

    Update Project api is Created and also tested

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -3,11 +3,13 @@ import CreateProject from "../controllers/Projets/CreateProject";
 import authenticateToken from "../middlewares/authToken";
 import GetProjects from "../controllers/Projets/GetProjects";
 import GetSingleProject from "../controllers/Projets/GetSingleProject";
+import UpdateProject from "../controllers/Projets/UpdateProject";
 
 const ProjetRoute=express.Router();
 
 ProjetRoute.post('/',authenticateToken,CreateProject)
 ProjetRoute.get('/',authenticateToken,GetProjects)
 ProjetRoute.get('/:projectId',authenticateToken,GetSingleProject)
+ProjetRoute.patch('/:projectId',authenticateToken,UpdateProject)
 
 export default ProjetRoute;
