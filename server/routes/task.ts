4e7ee2cdf2456b5f7dbf74f8c2ba77b071cbc301
Rahul Kumar <rahulrp8677@gmail.com>
2026-03-28 00:08:16 +0530

    Update Task Api was Created

--- server/routes/task.ts
+++ server/routes/task.ts
@@ -2,10 +2,14 @@ import express from "express";
 import authenticateToken from "../middlewares/authToken";
 import CreateTask from "../controllers/Task/CreateTask";
 import GetTasks from "../controllers/Task/GetTask";
+import GetSingleTask from "../controllers/Task/GetSingleTaskDetails";
+import UpdateTask from "../controllers/Task/UpdateTask";
 
 const TaskRoute=express.Router();
 
 TaskRoute.post('/',authenticateToken,CreateTask);
 TaskRoute.get('/',authenticateToken,GetTasks);
+TaskRoute.get('/:taskId',authenticateToken,GetSingleTask);
+TaskRoute.patch('/:taskId',authenticateToken,UpdateTask);
 
 export default TaskRoute;
